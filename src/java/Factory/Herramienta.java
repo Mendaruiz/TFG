514ff92c11f3b09389f49890bc807ba7af35f09d
David <David@192.168.1.18>
2017-05-08 10:35:57 +0200

    Las 3 herramientas incluidas, falta por poner el selector en el formulario y las diferentes salidas

---
+++ src/java/Factory/Herramienta.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Factory;
+
+import java.io.File;
+
+/**
+ *
+ * @author David
+ */
+public interface Herramienta {
+    public File XML();
+    public File XSL();
+}
